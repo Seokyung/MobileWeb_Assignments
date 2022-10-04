bce417c191d87cbd7e418f2419a1da12ecaf8014
seokyung <3skjee@naver.com>
2022-10-04 19:31:07 +0900

    실습일지6-2

--- 실습일지6/eventhandle/src/index.js
+++ 실습일지6/eventhandle/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Toggle from './toggle';
+import Lotto from './lotto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Toggle />
+  <Lotto />
 );
