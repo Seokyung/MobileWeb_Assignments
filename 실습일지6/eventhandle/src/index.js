6e0a512ad594302d46f6a69f987bacb98672086e
seokyung <3skjee@naver.com>
2022-10-05 15:39:42 +0900

    실습일지 6-2, 6-3 (주석달기까지 완료)

--- 실습일지6/eventhandle/src/index.js
+++ 실습일지6/eventhandle/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Toggle from './toggle';
 import Lotto from './lotto';
+import LoginControl from './loginCtrl';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Lotto />
+  <LoginControl />
 );
