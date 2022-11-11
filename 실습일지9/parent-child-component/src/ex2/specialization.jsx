5137175a0cc519bdb971a360470bf7ddfb878221
seokyung <3skjee@naver.com>
2022-11-12 02:06:25 +0900

    실습일지 9-1 수정

--- 실습일지9/parent-child-component/src/ex2/specialization.jsx
+++ 실습일지9/parent-child-component/src/ex2/specialization.jsx
@@ -9,6 +9,7 @@ function Dialog(props) {
             <div className={`${props.style}`}>{/** 다이얼로그의 구체화할 부분을 감싸는 div태그(props로 스타일을 전달받음) */}
                 <h3 className="dialog-title">{props.title}</h3>{/** props로 다이얼로그 제목을 전달받음 */}
                 <p className="dialog-content">{props.content}</p>{/** props로 다이얼로그 내용을 전달받음 */}
+                {props.btnTxt && <button className="dialog-button" onClick={props.onBtnClick}>{props.btnTxt}</button>}
             </div>
         </div>
     );
@@ -40,12 +41,17 @@ function GreetDialog() {
 
 //범용 다이얼로그 컴포넌트를 구체화시켜 만든 오류 다이얼로그 컴포넌트
 function ErrorDialog() {
+    const solveError = () => {
+        alert("오류가 해결되었습니다!");
+    }
     return(
         //범용 다이얼로그에 props를 보내 오류 다이얼로그에 맞게 컴포넌트를 구체화함
         <Dialog
             style="dialog-error"
             title="🚫 오류 발생!"
             content="오류 다이얼로그입니다."
+            btnTxt="오류고치기"
+            onBtnClick={solveError}
         />
     );
 }
